70f55cd1472b272ea4d166eb40013cd6ff61a04a
tombola <tombola@github>
2025-02-04 10:39:08 +0000

    Use stamina for retries

--- tests/test_resources.py
+++ tests/test_resources.py
@@ -1,25 +1,28 @@
 import json  # noqa: D100
 import os
 from pathlib import Path
-
 import responses
-
 from woocommerce_pydantic.wcapi.wc_api import API
 from woocommerce_pydantic.wcapi.models import wc_collections, wc_resources
 
 WC_URL = os.environ.get("TEST_WC_URL", "http://example.com")
-WC_API_URL = f"{WC_URL}/wp-json/wc/v3"
+WC_API_VERSION = os.environ.get("TEST_WC_URL", "http://example.com")
+WC_API_URL = f"{WC_URL}/wp-json/{WC_API_VERSION}"
+WC_API_KEY = os.environ.get("TEST_WC_KEY", "ck_XXXXXXXX")
+WC_API_SECRET = os.environ.get("TEST_WC_SECRET", "cs_XXXXXXXX"),
+
 
 # Currently tests do not use the API tokens as mocks using responses
 wcapi = API(
-    url=os.environ.get("TEST_WC_URL", "http://example.com"),
-    consumer_key=os.environ.get("TEST_WC_KEY", "ck_XXXXXXXX"),
-    consumer_secret=os.environ.get("TEST_WC_SECRET", "cs_XXXXXXXX"),
+    url=WC_URL,
+    consumer_key=WC_API_KEY,
+    consumer_secret=WC_API_SECRET,
     version="wc/v3",
 )
 
 # Get the local directory of the current file
-current_directory = os.path.dirname(os.path.abspath(__file__))
+# CURRENT_DIRECTORY = Path.parent(Path.resolve(__file__)
+CURRENT_DIRECTORY = os.path.dirname(os.path.abspath(__file__))
 
 def mock_wcapi_get(endpoint: str) -> dict:
     """
@@ -40,7 +43,7 @@ def mock_wcapi_get(endpoint: str) -> dict:
     """
     underscore_filename = endpoint.lstrip('/').replace('/', '_')
     example_file = f"data/responses/v3/{underscore_filename}.json"
-    with (Path(current_directory) / example_file).open() as f:
+    with (Path(CURRENT_DIRECTORY) / example_file).open() as f:
         data = json.load(f)
     responses.add(
         responses.GET,
